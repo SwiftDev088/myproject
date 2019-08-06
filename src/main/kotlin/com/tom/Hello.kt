60ea07a346f180c1caa6ca0b8c1bb45067718d64
swift <jms632068@gmail.com>
2019-08-06 22:32:18 +0800

    my test project

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,14 @@
+package com.tom
+
+fun main() {
+//    println("Hello")
+//    Human().hello()
+    val h=Human()
+    h.hello()
+}
+
+class Human {
+    fun hello(){
+        println("Hello")
+    }
+}
